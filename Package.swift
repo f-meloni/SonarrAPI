434663acd369266b8e2c5f6c8fa559ec88159abe
Franco Meloni <franco.meloni91@gmail.com>
2018-01-23 13:38:35 +0000

    Changed procedure kit version

--- Package.swift
+++ Package.swift
@@ -12,16 +12,15 @@ let package = Package(
             targets: ["SonarrAPI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.0.0"),
-        .package(url: "https://github.com/ProcedureKit/ProcedureKit.git", .branch("master"))
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: Version(4, 0, 0)),
+        .package(url: "https://github.com/ProcedureKit/ProcedureKit.git", from: Version(4, 4, 0))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SonarrAPI",
-            dependencies: ["Alamofire", "ProcedureKit"],
-            exclude:["TestingProcedureKit"]),
+            dependencies: ["Alamofire", "ProcedureKit"]),
         .testTarget(
             name: "SonarrAPITests",
             dependencies: ["SonarrAPI"]),
